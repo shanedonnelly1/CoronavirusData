84e9008a19b43eb6b0c01e7855c3031684059871
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-09-27 18:55:42 +0100

    Initial Commit

---
+++ CoronavirusData/CoronavirusDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronavirusDataApp.swift
+//  CoronavirusData
+//
+//  Created by Shane on 27/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct CoronavirusDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
